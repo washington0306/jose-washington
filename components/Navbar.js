6c84b0e40606c8f8cffea8e172f55b599091804d
José Washington Gomes <josewgomes0800@gmail.com>
2022-08-03 15:04:36 -0300

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => (
         <div className="navbar-fixed">
             <nav className="purple">
                 <div className="nav-wrapper container" style={{display:"flex", justifyContent:"space-around"}}>
-                    <a style={{padding:"0", margin:"0"}}>José Washington</a>
+                    <a style={{padding:"0", margin:"0", fontSize:"15px"}}>José Washington</a>
                     <a href="#" data-target="nav-mobile" className="right sidenav-trigger"><i className="material-icons">menu</i></a>
                     <ul className="right hide-on-med-and-down">
                         <li><a href="#Sobre">Sobre</a></li>
