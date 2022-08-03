774e576ed4ee6d8cce896c608f8379cf5cc32730
José Washington Gomes <josewgomes0800@gmail.com>
2022-08-03 18:26:53 -0300

    Update Home.js

--- components/Home.js
+++ components/Home.js
@@ -16,7 +16,7 @@ const Home = () => (
                                 <p style={{textAlign: "justify"}}>Programador ousado com conhecimentos de Java Script e React, apaixonado por tecnologia e programação. Graduando em Análise e desenvolvimento de sistemas na UNIFIP. Busco pôr em prática o que aprendi e continuo aprendendo no mundo da programação para criar códigos inovadores para os clientes da Empresa. Hoje me considero um profissional ainda em fase de aprendizado, porém com experiência e muita vontade de crescer.</p>
                             </div>
                             <div className="card-action size-screen-card">
-                                <a href="file:///C:/Users/55839/Downloads/C%C3%B3pia%20de%20cv101%20(1).pdf" className="waves-effect waves-light btn purple"><i className="material-icons left">cloud_download</i>Baixar currículo</a>
+                                <a href="https://docs.google.com/presentation/d/1aWNfynV9vq9pDTxxbGs2dJurrcXsb_exsmDX-GC-Y4A/edit#slide=id.p1" className="waves-effect waves-light btn purple"><i className="material-icons left">cloud_download</i>Baixar currículo</a>
                             </div>
                         </div>
                     </div>
